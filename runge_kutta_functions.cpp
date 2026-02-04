b5552f125eaaa404e11d0922bcd10ccc05c6860d
Rahul Kanna.A.V <91176771+RahulKannaAV@users.noreply.github.com>
2026-02-04 15:54:10 +0530

    Update code for 2nd Order Susceptible Function

--- runge_kutta_functions.cpp
+++ runge_kutta_functions.cpp
@@ -15,13 +15,52 @@ using namespace std;
 #define I0 481 // Initial Infected Count
 #define R0 487 // Initial Recovered Count
 
-
+// First order RK
 int k1_S() {
 	return (A - (alpha * S0 * I0) - (mew * S0) - (Vp * S0) - (Vf * S0));
 }
 
+int k1_E() {
+	return ((alpha * S0 * I0) - (mew * E0) - (delta * E0) + (Vp * S0));
+}
+
+int k1_I() {
+	return ((delta * E0) - (mew * I0) - (mew1 * I0) - (gamma * I0));
+}
+
+int k1_R() {
+	return ((Vf * S0) + (gamma * I0) - (mew * R0));
+}
+
+// Second Order RK
+int k2_S(float step_time) {
+	int term1_2, term3, term4_5;
+
+	int k1s_val = k1_S();
+	int k1i_val = k1_I();
+	
+	float delT_by2 = step_time / 2; 
+	term1_2 = A - (alpha * (S0 + (k1s_val * delT_by2)) * (I0 + (k1i_val * delT_by2)));
+	term3 = -mew * (S0 + (k1s_val * delT_by2));
+	term4_5 = (-Vp * (S0 + (k1s_val * delT_by2))) - (Vf * (S0 + (k1s_val * delT_by2)));
+	
+	return term1_2 + term3 + term4_5;
+}
+
 int main() {
 	int k1s_value = k1_S();
-	cout << k1s_value << endl;
+	int k1e_value = k1_E();
+	int k1i_value = k1_I();
+	int k1r_value = k1_R();
+	float step_time = 0.1;
+	
+	int k2s_value = k2_S(step_time);
+	
+	cout << "K1 for S: " << k1s_value << endl;
+	cout << "K1 for E: " << k1e_value << endl;
+	cout << "K1 for I: " << k1i_value << endl;
+	cout << "K1 for R: " << k1r_value << endl;
+	
+	cout << "\nK2 for S: " << k2s_value << endl;
 	return 0;
 }
