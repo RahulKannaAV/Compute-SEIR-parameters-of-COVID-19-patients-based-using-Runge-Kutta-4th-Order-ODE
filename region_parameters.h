58c2d89fa70d5f2491fe10269618ea2eb77364d9
Nickobellic <avrahulkanna17@gmail.com>
2026-03-12 09:51:51 +0530

    Add for multi regions before dividing N0

--- region_parameters.h
+++ region_parameters.h
@@ -4,7 +4,7 @@ using namespace std;
 
 // g++ test.cpp -DREGION=\"TAMIL_NADU\"
 
-	unordered_map<string, float> parameter_trivandrum = {
+	unordered_map<string, double> parameter_trivandrum = {
 		{"A", 33595},
 		{"alpha", 0.00001},
 		{"delta", 1.01},
@@ -20,7 +20,7 @@ using namespace std;
 		{"Vf", 0.1517}
 	};
 	
-	unordered_map<string, float> parameter_tamilnadu = {
+	unordered_map<string, double> parameter_tamilnadu = {
 		{"A", 1500000},
 		{"alpha", 0.00001},
 		{"delta", 1.2},
@@ -36,7 +36,7 @@ using namespace std;
 		{"Vf", 0.10}
 	};
 	
-	unordered_map<string, float> parameter_maharashtra = {
+	unordered_map<string, double> parameter_maharashtra = {
 		{"A", 2500000},
 		{"alpha", 0.00001},
 		{"delta", 1.35},
@@ -52,7 +52,7 @@ using namespace std;
 		{"Vf", 0.09}
 	};
 
-	unordered_map<string, float> parameter_gujarat = {
+	unordered_map<string, double> parameter_gujarat = {
     	{"A", 1100000},
     	{"alpha", 0.00001},
 		{"delta", 1.25},
@@ -68,7 +68,7 @@ using namespace std;
 		{"Vf", 0.095}
 	};
 	
-	unordered_map<string, float> parameter_delhi = {
+	unordered_map<string, double> parameter_delhi = {
     {"A", 900000},
     {"alpha", 0.00001},
     {"delta", 1.4},
@@ -84,11 +84,256 @@ using namespace std;
     {"Vf", 0.11}
 	};
 
-	inline unordered_map<string, unordered_map<string, float>> seir_params = {
+	unordered_map<string, double> parameter_karnataka = {
+    {"A", 1800000},
+    {"alpha", 0.00001},
+    {"delta", 1.30},
+    {"gamma", 0.83},
+    {"mew", 0.148},
+    {"mew1", 0.165},
+    {"N0", 67500000},
+    {"S0", 66000000},
+    {"E0", 120000},
+    {"I0", 210000},
+    {"R0", 1180000},
+    {"Vp", 0.21},
+    {"Vf", 0.11}
+};
+
+unordered_map<string, double> parameter_uttarpradesh = {
+    {"A", 3500000},
+    {"alpha", 0.00001},
+    {"delta", 1.28},
+    {"gamma", 0.82},
+    {"mew", 0.152},
+    {"mew1", 0.168},
+    {"N0", 230000000},
+    {"S0", 226000000},
+    {"E0", 300000},
+    {"I0", 450000},
+    {"R0", 2400000},
+    {"Vp", 0.17},
+    {"Vf", 0.08}
+};
+
+unordered_map<string, double> parameter_westbengal = {
+    {"A", 1600000},
+    {"alpha", 0.00001},
+    {"delta", 1.27},
+    {"gamma", 0.83},
+    {"mew", 0.146},
+    {"mew1", 0.163},
+    {"N0", 100000000},
+    {"S0", 98500000},
+    {"E0", 150000},
+    {"I0", 220000},
+    {"R0", 980000},
+    {"Vp", 0.19},
+    {"Vf", 0.10}
+};
+
+unordered_map<string, double> parameter_rajasthan = {
+    {"A", 1400000},
+    {"alpha", 0.00001},
+    {"delta", 1.24},
+    {"gamma", 0.84},
+    {"mew", 0.144},
+    {"mew1", 0.160},
+    {"N0", 81000000},
+    {"S0", 79800000},
+    {"E0", 80000},
+    {"I0", 150000},
+    {"R0", 850000},
+    {"Vp", 0.20},
+    {"Vf", 0.10}
+};
+
+unordered_map<string, double> parameter_madhyapradesh = {
+    {"A", 1300000},
+    {"alpha", 0.00001},
+    {"delta", 1.26},
+    {"gamma", 0.83},
+    {"mew", 0.147},
+    {"mew1", 0.162},
+    {"N0", 85000000},
+    {"S0", 83500000},
+    {"E0", 90000},
+    {"I0", 170000},
+    {"R0", 750000},
+    {"Vp", 0.18},
+    {"Vf", 0.09}
+};
+
+unordered_map<string, double> parameter_andhrapradesh = {
+    {"A", 1500000},
+    {"alpha", 0.00001},
+    {"delta", 1.29},
+    {"gamma", 0.84},
+    {"mew", 0.145},
+    {"mew1", 0.160},
+    {"N0", 54000000},
+    {"S0", 52500000},
+    {"E0", 85000},
+    {"I0", 160000},
+    {"R0", 720000},
+    {"Vp", 0.21},
+    {"Vf", 0.11}
+};
+
+unordered_map<string, double> parameter_telangana = {
+    {"A", 900000},
+    {"alpha", 0.00001},
+    {"delta", 1.28},
+    {"gamma", 0.84},
+    {"mew", 0.146},
+    {"mew1", 0.162},
+    {"N0", 40000000},
+    {"S0", 38900000},
+    {"E0", 65000},
+    {"I0", 120000},
+    {"R0", 500000},
+    {"Vp", 0.22},
+    {"Vf", 0.12}
+};
+
+unordered_map<string,double> parameter_bihar = {
+	{"A",2400000},{"alpha",0.00001},{"delta",1.28},{"gamma",0.83},{"mew",0.148},{"mew1",0.166},
+	{"N0",124000000},{"S0",122000000},{"E0",140000},{"I0",210000},{"R0",820000},{"Vp",0.15},{"Vf",0.07}
+};
+
+unordered_map<string,double> parameter_odisha = {
+	{"A",700000},{"alpha",0.00001},{"delta",1.22},{"gamma",0.85},{"mew",0.142},{"mew1",0.158},
+	{"N0",46000000},{"S0",45000000},{"E0",42000},{"I0",95000},{"R0",410000},{"Vp",0.19},{"Vf",0.10}
+};
+
+unordered_map<string,double> parameter_punjab = {
+	{"A",600000},{"alpha",0.00001},{"delta",1.26},{"gamma",0.84},{"mew",0.147},{"mew1",0.161},
+	{"N0",30000000},{"S0",29300000},{"E0",35000},{"I0",70000},{"R0",310000},{"Vp",0.22},{"Vf",0.12}
+};
+
+unordered_map<string,double> parameter_haryana = {
+	{"A",550000},{"alpha",0.00001},{"delta",1.27},{"gamma",0.84},{"mew",0.146},{"mew1",0.160},
+	{"N0",29000000},{"S0",28300000},{"E0",31000},{"I0",65000},{"R0",280000},{"Vp",0.23},{"Vf",0.12}
+};
+
+unordered_map<string,double> parameter_assam = {
+	{"A",500000},{"alpha",0.00001},{"delta",1.20},{"gamma",0.86},{"mew",0.140},{"mew1",0.156},
+	{"N0",35000000},{"S0",34400000},{"E0",20000},{"I0",45000},{"R0",150000},{"Vp",0.17},{"Vf",0.09}
+};
+
+unordered_map<string,double> parameter_jharkhand = {
+{"A",520000},{"alpha",0.00001},{"delta",1.23},{"gamma",0.85},{"mew",0.143},{"mew1",0.158},
+{"N0",39000000},{"S0",38200000},{"E0",28000},{"I0",56000},{"R0",210000},{"Vp",0.18},{"Vf",0.09}
+};
+
+/*
+unordered_map<string,double> parameter_chhattisgarh = {
+	{"A",480000},{"alpha",0.00001},{"delta",1.24},{"gamma",0.85},{"mew",0.144},{"mew1",0.159},
+	{"N0",30000000},{"S0",29200000},{"E0",32000},{"I0",61000},{"R0",170000},{"Vp",0.18},{"Vf",0.09}
+};
+
+unordered_map<string,double> parameter_himachal = {
+{"A",120000},{"alpha",0.00001},{"delta",1.15},{"gamma",0.88},{"mew",0.135},{"mew1",0.150},
+{"N0",7400000},{"S0",7200000},{"E0",4000},{"I0",9000},{"R0",28000},{"Vp",0.21},{"Vf",0.12}
+};
+
+unordered_map<string,double> parameter_uttarakhand = {
+	{"A",150000},{"alpha",0.00001},{"delta",1.18},{"gamma",0.87},{"mew",0.137},{"mew1",0.152},
+	{"N0",11200000},{"S0",10800000},{"E0",6000},{"I0",12000},{"R0",42000},{"Vp",0.22},{"Vf",0.12}
+};
+*/
+
+unordered_map<string,double> parameter_usa = {
+	{"A",8000000},{"alpha",0.00001},{"delta",1.35},{"gamma",0.82},{"mew",0.155},{"mew1",0.170},
+	{"N0",331000000},{"S0",320000000},{"E0",800000},{"I0",1200000},{"R0",8000000},{"Vp",0.45},{"Vf",0.35}
+};
+
+unordered_map<string,double> parameter_uk = {
+	{"A",1500000},{"alpha",0.00001},{"delta",1.30},{"gamma",0.84},{"mew",0.150},{"mew1",0.165},
+	{"N0",67000000},{"S0",63000000},{"E0",90000},{"I0",180000},{"R0",1500000},{"Vp",0.50},{"Vf",0.40}
+};
+
+unordered_map<string,double> parameter_germany = {
+	{"A",1200000},{"alpha",0.00001},{"delta",1.28},{"gamma",0.84},{"mew",0.148},{"mew1",0.163},
+	{"N0",83000000},{"S0",79000000},{"E0",110000},{"I0",210000},{"R0",1400000},{"Vp",0.48},{"Vf",0.38}
+};
+
+unordered_map<string,double> parameter_france = {
+	{"A",1100000},{"alpha",0.00001},{"delta",1.27},{"gamma",0.84},{"mew",0.148},{"mew1",0.163},
+	{"N0",67000000},{"S0",64000000},{"E0",95000},{"I0",180000},{"R0",1200000},{"Vp",0.46},{"Vf",0.36}
+};
+
+unordered_map<string,double> parameter_brazil = {
+	{"A",4000000},{"alpha",0.00001},{"delta",1.33},{"gamma",0.83},{"mew",0.152},{"mew1",0.168},
+	{"N0",213000000},{"S0",205000000},{"E0",420000},{"I0",780000},{"R0",3500000},{"Vp",0.30},{"Vf",0.20}
+};
+
+unordered_map<string,double> parameter_russia = {
+	{"A",2500000},{"alpha",0.00001},{"delta",1.29},{"gamma",0.83},{"mew",0.150},{"mew1",0.165},
+	{"N0",146000000},{"S0",140000000},{"E0",250000},{"I0",420000},{"R0",1800000},{"Vp",0.28},{"Vf",0.18}
+};
+
+unordered_map<string,double> parameter_china = {
+	{"A",10000000},{"alpha",0.00001},{"delta",1.10},{"gamma",0.90},{"mew",0.120},{"mew1",0.140},
+	{"N0",1440000000},{"S0",1430000000},{"E0",20000},{"I0",35000},{"R0",600000},{"Vp",0.60},{"Vf",0.50}
+};
+
+unordered_map<string,double> parameter_japan = {
+	{"A",1800000},{"alpha",0.00001},{"delta",1.20},{"gamma",0.88},{"mew",0.135},{"mew1",0.150},
+	{"N0",125000000},{"S0",120000000},{"E0",80000},{"I0",150000},{"R0",900000},{"Vp",0.52},{"Vf",0.40}
+};
+
+unordered_map<string,double> parameter_australia = {
+	{"A",500000},{"alpha",0.00001},{"delta",1.05},{"gamma",0.92},{"mew",0.110},{"mew1",0.130},
+	{"N0",25600000},{"S0",25200000},{"E0",5000},{"I0",9000},{"R0",45000},{"Vp",0.55},{"Vf",0.45}
+};
+
+unordered_map<string,double> parameter_canada = {
+    {"A",700000},{"alpha",0.00001},{"delta",1.22},{"gamma",0.86},{"mew",0.140},{"mew1",0.155},
+    {"N0",38000000},{"S0",37000000},{"E0",25000},{"I0",45000},{"R0",320000},{"Vp",0.55},{"Vf",0.45}
+};
+
+unordered_map<string,double> parameter_italy = {
+    {"A",1200000},{"alpha",0.00001},{"delta",1.28},{"gamma",0.84},{"mew",0.148},{"mew1",0.162},
+    {"N0",60000000},{"S0",57000000},{"E0",85000},{"I0",160000},{"R0",1200000},{"Vp",0.50},{"Vf",0.38}
+};
+
+unordered_map<string,double> parameter_southafrica = {
+    {"A",1000000},{"alpha",0.00001},{"delta",1.30},{"gamma",0.83},{"mew",0.150},{"mew1",0.165},
+    {"N0",60000000},{"S0",57500000},{"E0",90000},{"I0",150000},{"R0",850000},{"Vp",0.28},{"Vf",0.18}
+};
+
+inline unordered_map<string, unordered_map<string, double>> seir_params = {
 		{"TAMIL_NADU", parameter_tamilnadu},
 		{"MAHARASHTRA", parameter_maharashtra},
 		{"GUJARAT", parameter_gujarat},
 		{"KERALA", parameter_trivandrum},
-		{"DELHI", parameter_delhi}
+		{"DELHI", parameter_delhi},
+		{"KARNATAKA", parameter_karnataka},
+		{"UTTAR_PRADESH", parameter_uttarpradesh},
+		{"WEST_BENGAL", parameter_westbengal},
+		{"RAJASTHAN", parameter_rajasthan},
+		{"MADHYA_PRADESH", parameter_madhyapradesh},
+		{"ANDHRA_PRADESH", parameter_andhrapradesh},
+		{"TELANGANA", parameter_telangana},
+		{"BIHAR", parameter_bihar},
+		{"ODISHA", parameter_odisha},
+		{"PUNJAB", parameter_punjab},
+		{"HARYANA", parameter_haryana},
+		{"ASSAM", parameter_assam},
+		{"JHARKHAND", parameter_jharkhand},
+		{"USA", parameter_usa},
+		{"UK", parameter_uk},
+		{"GERMANY", parameter_germany},
+		{"FRANCE", parameter_france},
+		{"BRAZIL", parameter_brazil},
+		{"RUSSIA", parameter_russia},
+		{"CHINA", parameter_china},
+		{"JAPAN", parameter_japan},
+		{"AUSTRALIA", parameter_australia},
+		{"CANADA", parameter_canada},
+		{"ITALY", parameter_italy},
+		{"SOUTH_AFRICA", parameter_southafrica},
 	};
+	
 	
---
+++ runge_kutta_multi_region_seir.cpp
@@ -0,0 +1,533 @@
+#include<iostream>
+#include<vector>
+#include<iomanip>
+#include "region_parameters.h"
+using namespace std;
+
+/*
+#define A 33595 // Recruitment Rate
+#define alpha 0.00001 // Rate of Interaction  between susceptible and infected individuals
+#define delta 1.01 // Rate at which exposed population (E) becomes Infected population (I)
+#define gamma 0.9 // Rate of Medication to Recovery
+#define mew 0.143 // Natural Death Rate
+#define mew1 0.1595 // Disease related Deaths
+#define N0 1679754 // Initial total Population
+#define S0 1661989 // Initial Susceptible Count
+#define E0 16797 // Initial Exposed Count
+#define I0 481 // Initial Infected Count
+#define R0 487 // Initial Recovered Count
+#define Vp 0.2522 // Partially Vaccinated Population Count
+#define Vf 0.1517 // Fully Vaccinated Population Count
+*/
+
+// First order RK
+double k1_S(string region) {
+	double A = seir_params[region]["A"];
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double Vp = seir_params[region]["Vp"];
+	double Vf = seir_params[region]["Vf"];
+
+	return (A - (alpha * S0 * I0) - (mew * S0) - (Vp * S0) - (Vf * S0));
+}
+
+double k1_E(string region) {
+	double A = seir_params[region]["A"];
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double E0 = seir_params[region]["E0"];
+	double delta = seir_params[region]["delta"];
+	double Vp = seir_params[region]["Vp"];
+
+	return ((alpha * S0 * I0) - (mew * E0) - (delta * E0) + (Vp * S0));
+}
+
+
+double k1_I(string region) {
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double mew1 = seir_params[region]["mew1"];
+	double E0 = seir_params[region]["E0"];
+	double delta = seir_params[region]["delta"];
+	double gamma = seir_params[region]["gamma"];
+
+	return ((delta * E0) - (mew * I0) - (mew1 * I0) - (gamma * I0));
+}
+
+double k1_R(string region) {
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double R0 = seir_params[region]["R0"];
+	double Vf = seir_params[region]["Vf"];
+	double mew = seir_params[region]["mew"];
+	double gamma = seir_params[region]["gamma"];
+
+	return ((Vf * S0) + (gamma * I0) - (mew * R0));
+}
+
+// Second Order RK
+double k2_S(string region, float delT_by2) {
+	double term1_2, term3, term4_5;
+
+	double k1s_val = k1_S(region);
+	double k1i_val = k1_I(region);
+
+	double A = seir_params[region]["A"];
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double Vp = seir_params[region]["Vp"];
+	double Vf = seir_params[region]["Vf"];
+
+	// float delT_by2 = step_time / 2; 
+	term1_2 = A - (alpha * (S0 + (k1s_val * delT_by2)) * (I0 + (k1i_val * delT_by2)));
+	term3 = -mew * (S0 + (k1s_val * delT_by2));
+	term4_5 = (-Vp * (S0 + (k1s_val * delT_by2))) - (Vf * (S0 + (k1s_val * delT_by2)));
+	
+	return term1_2 + term3 + term4_5;
+}
+
+double k2_E(string region, double halved_step_time) {
+	double term1, term2, term3, term4;
+	
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double E0 = seir_params[region]["E0"];
+	double mew = seir_params[region]["mew"];
+	double delta = seir_params[region]["delta"];
+	double Vp = seir_params[region]["Vp"];
+	
+	double k1S = k1_S(region);
+	double k1I = k1_I(region);
+	double k1E = k1_E(region);
+
+	term1 = alpha * (S0 + (k1S*halved_step_time))*(I0 + (k1I * halved_step_time));
+	term2 = -mew * (E0 + (k1E * halved_step_time));
+	term3 = -delta * (E0 + (k1E *halved_step_time));
+	term4 = Vp * (S0 + (k1S*halved_step_time));
+
+	return term1 + term2 + term3 + term4;
+}
+
+double k2_I(string region, double halved_step_time) {
+	double term1, term2, term3, term4;
+	double delta = seir_params[region]["delta"];
+	double E0 = seir_params[region]["E0"];
+	double mew = seir_params[region]["mew"];
+	double I0 = seir_params[region]["I0"];
+	double mew1 = seir_params[region]["mew1"];
+	double gamma = seir_params[region]["gamma"];
+	
+	double k1E = k1_E(region);
+	double k1I = k1_I(region);
+
+	term1 = delta * (E0 + (k1E * halved_step_time));
+	term2 = -mew * (I0 + (k1I * halved_step_time));
+	term3 = -mew1 * (I0 + (k1I * halved_step_time));
+	term4 = -gamma * (I0 + (k1I * halved_step_time));
+
+	return term1 + term2 + term3 + term4;
+}
+
+double k2_R(string region, double halved_step_time) {
+	double term1, term2, term3;
+	double k1S = k1_S(region);
+	double k1I = k1_I(region);
+	double k1R = k1_R(region);
+	
+	double Vf = seir_params[region]["Vf"];
+	double S0 = seir_params[region]["S0"];
+	double gamma = seir_params[region]["gamma"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double R0 = seir_params[region]["R0"];
+
+	term1 = Vf*(S0 + (k1S * halved_step_time));
+	term2 = gamma * (I0 + (k1I * halved_step_time));
+	term3 = -mew * (R0 + (k1R * halved_step_time));
+
+	return term1 + term2 + term3;
+}
+
+// Third order RK
+double k3_S(string region, double halved_step_time) {
+	double term1, term2, term3, term4;
+	
+	double k2S = k2_S(region, halved_step_time);
+	double k2I = k2_I(region, halved_step_time);
+	
+	double A = seir_params[region]["A"];
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double Vp = seir_params[region]["Vp"];
+	double Vf = seir_params[region]["Vf"];
+	
+	term1 = -alpha*(S0+(k2S * halved_step_time)) * (I0 + (k2I * halved_step_time));
+	term2 = -mew * (S0 + (k2S * halved_step_time));
+	term3 = -Vp * (S0 + (k2S * halved_step_time));
+	term4 = -Vf * (S0 + (k2S * halved_step_time));
+	
+	
+	return A + term1 + term2 + term3 + term4;
+}
+
+double k3_E(string region, double halved_step_time) {
+	double term1, term2, term3, term4;
+	
+	double k2S = k2_S(region, halved_step_time);
+	double k2I = k2_I(region, halved_step_time);
+	double k2E = k2_E(region, halved_step_time);
+	
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double E0 = seir_params[region]["E0"];
+	double delta = seir_params[region]["delta"];
+	double Vp = seir_params[region]["Vp"];
+	
+	term1 = alpha * (S0 + (k2S * halved_step_time)) * (I0 + (k2I * halved_step_time));
+	term2 = -mew * (E0 + (k2E * halved_step_time));
+	term3 = -delta * (E0 + (k2E * halved_step_time));
+	term4 = Vp * (S0 + (k2S * halved_step_time));
+	
+	return term1 + term2 + term3 + term4;
+}
+
+double k3_I(string region, double halved_step_time) {
+	double term1, term2, term3, term4;
+	
+	double k2E = k2_E(region, halved_step_time);
+	double k2I = k2_I(region, halved_step_time);
+	
+	double delta = seir_params[region]["delta"];
+	double E0 = seir_params[region]["E0"];
+	double mew = seir_params[region]["mew"];
+	double I0 = seir_params[region]["I0"];
+	double mew1 = seir_params[region]["mew1"];
+	double gamma = seir_params[region]["gamma"];
+	
+	term1 = delta * (E0 + (k2E * halved_step_time));
+	term2 = -mew * (I0 + (k2I * halved_step_time));
+	term3 = -mew1 * (I0 + (k2I * halved_step_time));
+	term4 = -gamma * (I0 + (k2I * halved_step_time));
+	
+	return term1 + term2 + term3 + term4;
+}
+
+
+double k3_R(string region, double halved_step_time) {
+	double term1, term2, term3;
+	
+	double k2S = k2_S(region, halved_step_time);
+	double k2I = k2_I(region, halved_step_time);
+	double k2R = k2_R(region, halved_step_time);
+	
+	double Vf = seir_params[region]["Vf"];
+	double S0 = seir_params[region]["S0"];
+	double gamma = seir_params[region]["gamma"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double R0 = seir_params[region]["R0"];
+	
+	
+	term1 = Vf * (S0 + (k2S * halved_step_time));
+	term2 = gamma * (I0 + (k2I * halved_step_time));
+	term3 = -mew * (R0 + (k2R * halved_step_time));
+	
+	return term1 + term2 + term3;
+}
+
+// Fourth Order RK
+double k4_S(string region, double step_time) {
+	double term1, term2, term3, term4;
+	
+	double k3S = k3_S(region, step_time/2);
+	double k3I = k3_I(region, step_time/2);
+	
+	double A = seir_params[region]["A"];
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double Vp = seir_params[region]["Vp"];
+	double Vf = seir_params[region]["Vf"]; 
+
+	term1 = -alpha * (S0 + (k3S * step_time)) * (I0 + (k3I * step_time));
+	term2 = -mew * (S0 + (k3S * step_time));
+	term3 = -Vp * (S0 + (k3S * step_time));
+	term4 = -Vf * (S0 + (k3S * step_time));
+	
+	
+	return A + term1 + term2 + term3 + term4;
+}
+
+double k4_E(string region, double step_time) {
+	double term1, term2, term3, term4;
+	
+	double halved_step_time = step_time/2; 
+	double k3S = k3_S(region, halved_step_time);
+	double k3I = k3_I(region, halved_step_time);
+	double k3E = k3_E(region, halved_step_time);
+	
+	double alpha = seir_params[region]["alpha"];
+	double S0 = seir_params[region]["S0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double E0 = seir_params[region]["E0"];
+	double delta = seir_params[region]["delta"];
+	double Vp = seir_params[region]["Vp"];
+
+	term1 = alpha * (S0 + (k3S * step_time)) * (I0 + (k3I * step_time));
+	term2 = -mew * (E0 + (k3E * step_time));
+	term3 = -delta * (E0 + (k3E * step_time));
+	term4 = Vp * (S0 + (k3S * step_time));
+	
+	return term1 + term2 + term3 + term4;
+}
+
+double k4_I(string region, double step_time) {
+	double term1, term2, term3, term4;
+	
+	double halved_step_time = step_time / 2;
+	double k3E = k3_E(region, halved_step_time);
+	double k3I = k3_I(region, halved_step_time);
+
+	double delta = seir_params[region]["delta"];
+	double E0 = seir_params[region]["E0"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double mew1 = seir_params[region]["mew1"];
+	double gamma = seir_params[region]["gamma"];
+	
+	term1 = delta * (E0 + (k3E * step_time));
+	term2 = -mew * (I0 + (k3I * step_time));
+	term3 = -mew1 * (I0 + (k3I * step_time));
+	term4 = -gamma * (I0 + (k3I * step_time));
+	
+	return term1 + term2 + term3 + term4;
+}
+
+double k4_R(string region, double step_time) {
+	double term1, term2, term3;
+	double halved_step_time = step_time / 2;
+	
+	double k3S = k3_S(region, halved_step_time);
+	double k3I = k3_I(region, halved_step_time);
+	double k3R = k3_R(region, halved_step_time);
+
+	double Vf = seir_params[region]["Vf"];
+	double S0 = seir_params[region]["S0"];
+	double gamma = seir_params[region]["gamma"];
+	double I0 = seir_params[region]["I0"];
+	double mew = seir_params[region]["mew"];
+	double R0 = seir_params[region]["R0"];
+	
+	term1 = Vf * (S0 + (k3S * step_time));
+	term2 = gamma * (I0 + (k3I * step_time));
+	term3 = -mew * (R0 + (k3R * step_time));
+	
+	return term1 + term2 + term3;
+}
+
+// Computing next state
+double nextS(string region, double step_time) {
+	double halved_step_time = step_time / 2;
+	double currentS = seir_params[region]["S0"];
+	
+	double k1S = k1_S(region);
+	double k2S = k2_S(region, halved_step_time);
+	double k3S = k3_S(region, halved_step_time);
+	double k4S = k4_S(region, step_time);	
+	
+	double S1 = currentS + ((step_time/6) * (k1S + (2 * k2S) + (2 * k3S) + k4S));
+	
+	return S1;
+}
+
+double nextE(string region, double step_time) {
+	double halved_step_time = step_time / 2;
+	double currentE = seir_params[region]["E0"];
+
+	double k1E = k1_E(region);
+	double k2E = k2_E(region, halved_step_time);
+	double k3E = k3_E(region, halved_step_time);
+	double k4E = k4_E(region, step_time);
+	
+	double E1 = currentE + ((step_time/6) * (k1E + (2*k2E) + (2*k3E) + k4E));
+	
+	return E1;
+}
+
+double nextI(string region, double step_time) {
+	double halved_step_time = step_time / 2;
+	double currentI = seir_params[region]["I0"];
+	
+	double k1I = k1_I(region);
+	double k2I = k2_I(region, halved_step_time);
+	double k3I = k3_I(region, halved_step_time);
+	double k4I = k4_I(region, step_time);
+	
+	double I1 = currentI + ((step_time/6) * (k1I + (2*k2I) + (2*k3I) + k4I));
+	
+	return I1;
+}
+
+double nextR(string region, float step_time) {
+	double halved_step_time = step_time / 2;
+	double currentR = seir_params[region]["R0"];
+
+	double k1R = k1_R(region);
+	double k2R = k2_R(region, halved_step_time);
+	double k3R = k3_R(region, halved_step_time);
+	double k4R = k4_R(region, step_time);
+	
+	double R1 = currentR + ((step_time/6) * (k1R + (2*k2R) + (2*k3R) + k4R));
+	
+	return R1;
+}
+
+
+void computeSEIR(string region) {
+	double S0, E0, I0, R0;
+
+	S0 = seir_params[region]["S0"];
+	E0 = seir_params[region]["E0"];
+	I0 = seir_params[region]["I0"];
+	R0 = seir_params[region]["R0"];
+	double step_time = 0.01;
+	double half_step_time = step_time/2;
+
+	double s1 = nextS(region, step_time);
+	double e1 = nextE(region,  step_time);
+	double i1 = nextI(region,  step_time);
+	double r1 = nextR(region, step_time);
+
+	seir_params[region]["S0"] = s1;
+	seir_params[region]["E0"] = e1;
+	seir_params[region]["I0"] = i1;
+	seir_params[region]["R0"] = r1;
+}
+
+int main() {
+	cout << setprecision(0) << fixed;
+	double S0, E0, I0, R0;
+	string region;
+
+	vector<string> allRegions;
+
+	for(auto region_name: seir_params) {
+		allRegions.push_back(region_name.first);
+	}
+
+	for(string regions: allRegions) {
+		cout << regions << " ";
+	}
+	cout << endl << endl;
+
+/*
+	int k1s_value = k1_S(region);
+	int k1e_value = k1_E(region);
+	int k1i_value = k1_I(region);
+	int k1r_value = k1_R(region);
+	
+	int k2s_value = k2_S(region, step_time);
+	int k2e_value = k2_E(region, half_step_time);
+	int k2i_value = k2_I(region, half_step_time);
+	int k2r_value = k2_R(region, half_step_time);
+
+	
+	cout << "K1 for S: " << k1s_value << endl;
+	cout << "K1 for E: " << k1e_value << endl;
+	cout << "K1 for I: " << k1i_value << endl;
+	cout << "K1 for R: " << k1r_value << endl;
+	
+	cout << "\nK2 for S: " << k2s_value << endl;
+	cout << "K2 for E: " << k2e_value << endl;
+	cout << "K2 for I: " << k2i_value << endl;
+	cout << "K2 for R: " << k2r_value << endl;
+	
+	int k3s_value = k3_S(region, half_step_time);
+	int k3e_value = k3_E(region, half_step_time);
+	int k3i_value = k3_I(region, half_step_time);
+	int k3r_value = k3_R(region, half_step_time);
+
+	cout << "\nK3 for S: " << k3s_value << endl;
+	cout << "K3 for E: " << k3e_value << endl;
+	cout << "K3 for I: "  << k3i_value << endl;
+	cout << "K3 for R: " << k3r_value << endl;
+	
+	int k4s_value = k4_S(region, step_time);
+	int k4e_value = k4_E(region, step_time);
+	int k4i_value = k4_I(region, step_time);
+	int k4r_value = k4_R(region, step_time);
+	
+	cout << "\nK4 for S: " << k4s_value << endl;
+	cout << "K4 for E: " << k4e_value << endl;
+	cout << "K4 for I: " << k4i_value << endl;
+	cout << "K4 for R: " << k4r_value << endl;
+	
+	cout << "Initial: " << endl;
+
+	cout << "S" << 0 << ": " << S0 << endl;
+	cout << "E" << 0 << ": " << E0 << endl;
+	cout << "I" << 0 << ": " << I0 << endl;
+	cout << "R" << 0 << ": " << R0 << endl << endl;
+
+	cout << "\n10 States " << endl;
+*/
+cout << seir_params["KERALA"]["S0"] << endl;
+
+region = "KERALA";
+	for(int reg_idx=0; reg_idx < allRegions.size(); reg_idx++){
+		cout << "SEIR of region " << region << endl;
+		cout << "Initial Day 0: " << endl;
+		S0 = seir_params[region]["S0"];
+		E0 = seir_params[region]["E0"];
+		I0 = seir_params[region]["I0"];
+		R0 = seir_params[region]["R0"];
+						
+		cout << "S" << 0 << ": " << S0 << endl;
+		cout << "E" << 0 << ": " << E0 << endl;
+		cout << "I" << 0 << ": " << I0 << endl;
+		cout << "R" << 0 << ": " << R0 << endl << endl;
+
+		for(int i=2; i<=7; i++) {
+			S0 = seir_params[region]["S0"];
+			E0 = seir_params[region]["E0"];
+			I0 = seir_params[region]["I0"];
+			R0 = seir_params[region]["R0"];
+			
+			computeSEIR(region);
+			
+			if(i<=55){
+				cout << "Day " << i << endl;
+				cout << "S" << i << ": " << S0 << endl;
+				cout << "E" << i << ": " << E0 << endl;
+				cout << "I" << i << ": " << I0 << endl;
+				cout << "R" << i << ": " << R0 << endl << endl;
+			}
+	
+		}
+		break;
+	}
+	
+	/*
+	cout << setprecision(6) << fixed;
+	cout << "For Region " << region  << " after Day 9: "<< endl;
+	for(auto const &p: seir_params[region]) {
+		cout << p.first << " : " << p.second << endl;
+	}
+	*/
+	
+	return 0;
+}
+
